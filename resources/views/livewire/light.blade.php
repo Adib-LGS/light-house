29b03f158b9f2c1de0857b427aef9f1e1fefaedf
Adib-Legastelois <adib.legastelois@icloud.com>
2020-08-01 23:14:15 +0100

    Add progressive bar = btn bootstrap 4

--- resources/views/livewire/light.blade.php
+++ resources/views/livewire/light.blade.php
@@ -1,4 +1,18 @@
 <div>
     <h1>Light House</h1>
-    <h2>&#128161; {{ $brightness }}%</h2>
+        <h2>&#128161; {{ $brightness }}%</h2>
+
+    <div class="progress" style="height: 2rem;">
+  <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: {{ $brightness }}%" aria-valuenow="{{ $brightness }}" aria-valuemin="0" aria-valuemax="100"></div>
+    </div>
+
+    <div class="mt-3">
+        <button class="btn btn-danger">Off</button>
+   
+        <button class="btn btn-secondary">-</button>
+   
+        <button class="btn btn-secondary">+</button>
+    
+        <button class="btn btn-primary">On</button>
+    </div>
 </div>
