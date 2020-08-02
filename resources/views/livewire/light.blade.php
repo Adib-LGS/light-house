6a99e6aea49f62a2e7f416ab8f493273ffb0d11b
Adib-Legastelois <adib.legastelois@icloud.com>
2020-08-02 18:10:08 +0100

    little modif

--- resources/views/livewire/light.blade.php
+++ resources/views/livewire/light.blade.php
@@ -3,7 +3,7 @@
         <h2>&#128161; {{ $brightness }}%</h2>
 
     <div class="progress" style="height: 2rem;">
-  <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: {{ $brightness }}%" aria-valuenow="{{ $brightness }}" aria-valuemin="0" aria-valuemax="100"></div>
+  <div class="progress-bar progress-bar-striped bg-warning progress-bar-animated" role="progressbar" style="width: {{ $brightness }}%" aria-valuenow="{{ $brightness }}" aria-valuemin="0" aria-valuemax="100"></div>
     </div>
 
     <div class="mt-3">
